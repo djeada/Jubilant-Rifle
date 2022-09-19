101d9344d65deb60bb3c97eeb159dd4d69b52383
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-09-19 13:39:42 +0200

    Update vector.h

--- src/vector.h
+++ src/vector.h
@@ -7,15 +7,7 @@ typedef struct {
     int size;
     int maxSize;
     Type type;
-} Info;
-
-typedef struct {
-    int size;
-    int maxSize;
-    Type type;
     void** data;
 } Vector;
-
-//void append(Info *info, void** vector, void* data);
 
 void append(Vector* vector, void* newElement);
